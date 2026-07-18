3e480936672689baa737ea45ae2a23eb6338905d
NVAn02 <vanan22022002@gmail.com>
2026-07-19 02:49:16 +0700

    Update YouTubeSubtitleLearningToolApplicationTests.java

--- Backend/src/test/java/personal_project/personal_project/YouTubeSubtitleLearningToolApplicationTests.java
+++ Backend/src/test/java/personal_project/personal_project/YouTubeSubtitleLearningToolApplicationTests.java
@@ -2,8 +2,15 @@ package personal_project.personal_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 class YouTubeSubtitleLearningToolApplicationTests {
 
     @Test
